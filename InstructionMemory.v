28eb35a7d1065d7263a5097295d27d215a5d3d29
stealth07 <andres07@email.arizona.edu>
2016-12-04 13:50:42 -0700

    UPDATE 14: SAD_V8

--- InstructionMemory.v
+++ InstructionMemory.v
@@ -64,7 +64,8 @@ module InstructionMemory(Address, Instruction);
         //$readmemb("SAD_V4_Out.txt", memory);
         //$readmemb("SAD_V5_Out.txt", memory);
         //$readmemb("SAD_V6_Out.txt", memory);
-        $readmemb("SAD_V7_Out.txt", memory);
+        //$readmemb("SAD_V7_Out.txt", memory);
+        $readmemb("SAD_V8_Out.txt", memory);
     end
 
     assign Instruction = memory[Address[31:2]];
